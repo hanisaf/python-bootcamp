8467a41fd7ebf5de1f34d18c5865e6eefd6205c2
hanisaf <hanisaf@gmail.com>
2018-09-25 12:03:56 -0400

    Update install_local_jupyter.sh

--- install_local_jupyter.sh
+++ install_local_jupyter.sh
@@ -1,3 +1,11 @@
+# I suggest installing a local virtual environment
+python3 -m venv env
+# Then switch to that environment
+source env/bin/activate
+# Innstall requirments
+pip3 install -r requirements.txt
+# finally because jupyter has different configuration than the shell
+# we need to create a new kernel to map to the virtual environment
 jupyter kernelspec list
 ipython kernel install --user --name=bootcamp
 jupyter kernelspec list
